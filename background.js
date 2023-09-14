24dda0b75653a7000061d0d00f489336971a1c59
Shaun Ferris <slb.ferris46@gmail.com>
2023-09-14 11:50:19 +0100

    Start building out the real background script, porting over to browser namespace from chromse namespace Also revert eslint settings to inherit from reccommended

--- background.js
+++ background.js
@@ -1,11 +1,43 @@
-const testNotif = {
-  title: "test noftification",
-  type: "basic",
-  message: "This is a test of the notifications API",
-  iconUrl: "/icons/icon128.png"
-};
-
 browser.commands.onCommand.addListener(async (command) => {
   if (command === "convert_selection") {
+    const activeTab = await getTab();
+    const response = await browser.scripting.executeScript({
+      target: { tabId: activeTab.id },
+      func: convertAndCopy
+    });
+    const notifMsg = responseNotifications[response[0].result];
+    browser.notifications.create(notifMsg);
   }
 });
+
+async function getTab() {
+  return;
+}
+
+async function convertAndCopy() {
+  return;
+}
+
+const responseNotifications = {
+  success: {
+    title: "Successful spOnGEcAsE conversion!",
+    type: "basic",
+    message:
+      "The text you highlighted has been converted and copied to the clipboard, go ahead and paste it somewhere",
+    iconUrl: "icons/icon128.png"
+  },
+  fail: {
+    title: "Failed spOnGEcAsE conversion.",
+    type: "basic",
+    message:
+      "There was an error converting selected text, try reloading the extension, or check the developer console for more information.",
+    iconUrl: "icons/icon128.png"
+  },
+  noInput: {
+    title: "No text selected",
+    type: "basic",
+    message:
+      "You pushed the hotkey for the spongecase converter extension but had not text selected to convert!",
+    iconUrl: "/icons/icon128.png"
+  }
+};
